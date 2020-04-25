34d5fd02d1acac88ca5ba3ee1af19d627a8eb3c9
Poltorakov Vlad <poltorakov.v@2wtrade.com>
2020-04-25 15:11:22 +0700

    Добавляет отправку сообщений боту

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"parser/db/models"
 	_redis "parser/helpers/redis"
 	"parser/services"
 	"strconv"
+	"strings"
 )
 
 func init() {
@@ -44,30 +46,70 @@ func main() {
 
 		if isUnique {
 			newVacanciesCount++
-			// записать в мапу уже готовое сообщение для бота
+			err := sendToChannel(prepareMessage(&vacancy))
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 
 	if newVacanciesCount != 0 {
 		redisClient.SetRedisTimeStamp()
-
-		// кидаем уведомление боту
-		telegramToken, isExists := os.LookupEnv("TELEGRAM_BOT_TOKEN")
-		if !isExists {
-			panic(errors.New("telegram token not found"))
-		}
-
-		telegramChatID, isExists := os.LookupEnv("TELEGRAM_CHAT_ID")
-		if !isExists {
-			panic(errors.New("telegram chat id not found"))
-		}
-		bot, err := tgbotapi.NewBotAPI(telegramToken)
-		if err != nil {
-			panic(err)
-		}
-
-		chatId, _ := strconv.Atoi(telegramChatID)
-		msg := tgbotapi.NewMessage(int64(chatId), "aaaa")
-		bot.Send(msg)
 	}
+}
+
+func sendToChannel(msg string) error {
+	telegramToken, isExists := os.LookupEnv("TELEGRAM_BOT_TOKEN")
+	if !isExists {
+		panic(errors.New("telegram token not found"))
+	}
+
+	telegramChatID, isExists := os.LookupEnv("TELEGRAM_CHAT_ID")
+	if !isExists {
+		panic(errors.New("telegram chat id not found"))
+	}
+
+	bot, err := tgbotapi.NewBotAPI(telegramToken)
+	if err != nil {
+		panic(err)
+	}
+
+	chatId, _ := strconv.Atoi(telegramChatID)
+	message := tgbotapi.NewMessage(
+		int64(chatId),
+		msg,
+	)
+	_, err = bot.Send(message)
+	if err != nil {
+		log.Fatal(err)
+
+		return err
+	}
+
+	return nil
+}
+
+func prepareMessage(vacansy *models.Vacancy) string {
+	var str strings.Builder
+	str.WriteString(vacansy.Name + "\n")
+
+	if vacansy.Salary.From == 0 && vacansy.Salary.To == 0 {
+		str.WriteString("Зарплата: не указана\n")
+	} else if vacansy.Salary.From != 0 && vacansy.Salary.To == 0 {
+		str.WriteString("Зарплата: от " + strconv.Itoa(vacansy.Salary.From) + "\n")
+	} else if vacansy.Salary.To != 0 && vacansy.Salary.From == 0 {
+		str.WriteString("Зарплата: до " + strconv.Itoa(vacansy.Salary.To) + "\n")
+	} else {
+		str.WriteString("Зарплата: " +
+			"от " + strconv.Itoa(vacansy.Salary.From) +
+			" до " + strconv.Itoa(vacansy.Salary.To) +
+			"\n",
+		)
+	}
+
+	str.WriteString(vacansy.Snippet.Description + "\n")
+	str.WriteString("Требования: " + vacansy.Snippet.Requirements + "\n")
+	str.WriteString(vacansy.Employer.Name + "\n")
+
+	return str.String()
 }
